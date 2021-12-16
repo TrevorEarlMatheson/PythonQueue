fc9d5207a64760cc079f29426661f5a1ab811f3e
Trevor Matheson <trevor.earl.matheson@gmail.com>
2021-12-16 00:21:07 -0700

    Added method to remove item in the front of the queue.

--- queue.py
+++ queue.py
@@ -2,5 +2,10 @@ class Queue:
     def __init__(self):
         self.values = []
 
+    # Add values to the end of the queue.
     def enqueue(self, key):
         self.values.append(key)
+
+   # Remove value at the front of the queue.
+    def dequeue(self):
+        self.values.pop(0)
