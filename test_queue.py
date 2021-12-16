1d895370a7af9e5fbeba18057fc247374ff5dbc2
Trevor Matheson <trevor.earl.matheson@gmail.com>
2021-12-16 00:53:18 -0700

    Added test to check that we handle dequeue on an empty queue.

--- test_queue.py
+++ test_queue.py
@@ -9,3 +9,16 @@ class TestQueue(unittest.TestCase):
             queue.enqueue(value)
         self.assertEqual(values, queue.values, "Queue was " + str(queue.values) + " but should have been " + str(values))
 
+    def test_dequeue(self):
+        queue = Queue()
+        values = [3, 2, 5, 4, 7, 1, 5]
+        for value in values:
+            queue.enqueue(value)
+        self.assertEqual(values, queue.values, "Queue was " + str(queue.values) + " but should have been " + str(values))
+        queue.dequeue()
+        values.pop(0)
+        self.assertEqual(values, queue.values, "Queue was " + str(queue.values) + " but should have been " + str(values))
+
+    def test_dequeue_two(self):
+        queue = Queue()
+        queue.dequeue()
